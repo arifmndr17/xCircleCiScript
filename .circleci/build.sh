a07116b3aedb6899a8088212ed82eb21166388b0
ArifDroidDev <91365852+ArifDroidDev@users.noreply.github.com>
2021-09-25 18:59:06 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -36,8 +36,8 @@ echo ================================================
 function compile() {
 
    # Your Telegram Group
-   curl -s -X POST "https://api.telegram.org/bot$1275367356:AAGy9rCe3WVf7spx64fNmslEE10tzCejrjQ/sendMessage" \
-        -d chat_id="-1001599759389" \
+   curl -s -X POST "https://api.telegram.org/bot$1678018441:AAF5dhxlB216j3Cl4fnCZ09heng8R3Geyoc/sendMessage" \
+        -d chat_id="-1001287929514" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG VERSION : <code>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
@@ -61,8 +61,8 @@ function compile() {
 function push() {
     cd anyKernel
     ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
+    curl -F document=@$ZIP "https://api.telegram.org/bot$1678018441:AAF5dhxlB216j3Cl4fnCZ09heng8R3Geyoc/sendDocument" \
+        -F chat_id="-1001287929514" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
         -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7 (lavender)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
@@ -70,8 +70,8 @@ function push() {
 }
 # Fin Error
 function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
+    curl -s -X POST "https://api.telegram.org/bot$1678018441:AAF5dhxlB216j3Cl4fnCZ09heng8R3Geyoc/sendMessage" \
+        -d chat_id="-1001287929514" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
         -d text="Build throw an error(s)"
