bed2217a7d93a287199516d5e21a54b6b0269b50
ArifDroidDev <91365852+ArifDroidDev@users.noreply.github.com>
2021-09-25 16:53:55 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -36,14 +36,14 @@ echo ================================================
 function compile() {
 
    # Your Telegram Group
-   curl -s -X POST "https://api.telegram.org/bot$2037078975:AAE7HL26qDv6rslkKaHKYDeMD9RrhqVqxEk/sendMessage" \
+   curl -s -X POST "https://api.telegram.org/bot$1275367356:AAGy9rCe3WVf7spx64fNmslEE10tzCejrjQ/sendMessage" \
         -d chat_id="-1001599759389" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${arif}</code>%0ABUILDER HOST : <code>${mndr-ci}</code>%0ADEVICE DEFCONFIG : <code>${X01AD_defconfig}</code>%0ACLANG VERSION : <code>$(${clang}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${clang}</code>%0AKERNEL ROOTDIR : <code>${X01AD}</code>"
+        -d text="<b>xKernelCompiler</b>%0ABUILDER NAME : <code>${KBUILD_BUILD_USER}</code>%0ABUILDER HOST : <code>${KBUILD_BUILD_HOST}</code>%0ADEVICE DEFCONFIG : <code>${DEVICE_DEFCONFIG}</code>%0ACLANG VERSION : <code>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0ACLANG ROOTDIR : <code>${CLANG_ROOTDIR}</code>%0AKERNEL ROOTDIR : <code>${KERNEL_ROOTDIR}</code>"
 
-  cd {X01AD}
-  make -j$(nproc) O=out ARCH=arm64 {X01AD_defconfig}
+  cd ${KERNEL_ROOTDIR}
+  make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
   make -j$(nproc) ARCH=arm64 O=out \
 	CC=${CLANG_ROOTDIR}/bin/clang \
 	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
