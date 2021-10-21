45aed1e2298d8b4279be31f843896b98f2d611dc
ArifDroidDev <91365852+ArifDroidDev@users.noreply.github.com>
2021-10-21 19:00:17 +0700

    Create upstream.sh

---
+++ .circleci/upstream.sh
@@ -0,0 +1,3 @@
+git remote add upstream https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/ && git fetch upstream v4.9.287 && git merge FETCH_HEAD
+git fetch upstream v4.9.287 && git merge FETCH_HEAD
+git merge --continue
