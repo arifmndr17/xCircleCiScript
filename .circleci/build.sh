756e4ccedbf55724cfc1f34ce024413f54e71fcd
ArifDroidDev <91365852+ArifDroidDev@users.noreply.github.com>
2021-10-24 12:35:02 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -56,7 +56,7 @@ tg_post_msg "<b>Bot Test:</b><code>Membangun Kernel sedang di mulai, Sabar ya br
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
-    CC=${CLANG_ROOTDIR}/bin/clang \
+    CC=${CLANG_ROOTDIR}/bin/gcc \
     AR=${CLANG_ROOTDIR}/bin/llvm-ar \
     NM=${CLANG_ROOTDIR}/bin/llvm-nm \
     OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
